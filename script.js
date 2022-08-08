e4f0131737784910446aa8cacb158f72c87dbadd
OliviaDemaret <109947258+OliviaDemaret@users.noreply.github.com>
2022-08-08 15:24:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+{
+  let swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+})
+}
+ 
